1eefdc696411041a5393a25ddf07e7bedc95a1ea
fperret <fperret@student.42.fr>
2020-05-30 16:35:18 +0200

    basic object interaction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,6 +18,9 @@ public class Player : MonoBehaviour
     private float m_speed = 3.0f;
     private float m_raycastLength = 3.0f;
 
+    private GameObject m_currentTarget = null;
+    private Color      m_saveTargetColor;
+
     private Vector2 m_movement;
     // Simulate the direction like a 3D object would have
     private Vector2 m_direction;
@@ -48,11 +51,73 @@ public class Player : MonoBehaviour
         }
     }
 
+    // Update the game object of the new target (highlight, etc...)
+    private void updateNewTargetGameObject(GameObject newCurrentTarget)
+    {
+        m_currentTarget = newCurrentTarget;
+        m_saveTargetColor = m_currentTarget.GetComponent<SpriteRenderer>().color;
+
+        Color newColor = m_saveTargetColor;
+        newColor.r *= 1.3f;
+        newColor.g *= 1.3f;
+        newColor.b *= 1.3f;
+        m_currentTarget.GetComponent<SpriteRenderer>().color = newColor;
+    }
+
+    // Restore the game object of the previous target as it was before targetting.
+    // Should do the opposite of updateNewTargetGameObject()
+    private void restorePreviousTargetGameObject()
+    {
+        m_currentTarget.GetComponent<SpriteRenderer>().color = m_saveTargetColor;
+    }
+
+    // Get a target from a raycast in front of the player
+    private void target()
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(m_center.position, m_direction, m_raycastLength);
+        GameObject target = null;
+
+        Debug.DrawLine(m_center.position, m_center.position + (new Vector3(m_direction.x, m_direction.y) * m_raycastLength), Color.magenta);
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.tag == "Interactable")
+            {
+                // Colliders are sorted in order of distance so we can stop at the first one
+                target = hit.collider.gameObject;
+                break;
+            }
+        }
+
+        // No object to target
+        if (target == null)
+        {
+            // Restore the previous object we had as a target
+            if (m_currentTarget != null)
+                restorePreviousTargetGameObject();
+            m_currentTarget = null;
+        }
+
+        // We have possibly an objet to target
+        if (target != null)
+        {
+            // If no current target, use the new one
+            if (m_currentTarget == null)
+                updateNewTargetGameObject(target);
+            // Check if the target is different from currentTarget
+            else if (target.GetInstanceID() != m_currentTarget.GetInstanceID())
+            {
+                restorePreviousTargetGameObject();
+                updateNewTargetGameObject(target);
+            }
+        }
+
+    }
+
     private void FixedUpdate() {
         m_rigidBody.MovePosition(m_rigidBody.position + m_movement * m_speed * Time.fixedDeltaTime);    
 
-        RaycastHit2D hit = Physics2D.Raycast(m_center.position, m_direction, m_raycastLength);
-        Debug.DrawLine(m_center.position, m_center.position + (new Vector3(m_direction.x, m_direction.y) * m_raycastLength), Color.magenta);
+        target();
+
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -73,7 +138,6 @@ public class Player : MonoBehaviour
             newColor.g *= 0.9f;
             newColor.b *= 0.9f;
             other.GetComponent<SpriteRenderer>().color = newColor;
-         //   other.GetComponent<SpriteRenderer>().sprite = m_darkerSand;
         }        
     }
 
