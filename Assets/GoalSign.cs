db9cab18c6f42b29d0a06d87cad202fa38fcedad
fperret <fperret@student.42.fr>
2020-06-01 10:32:44 +0200

    goal model display when approaching sign

---
+++ Assets/GoalSign.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalSign : MonoBehaviour
+{
+    public GameObject  m_goalSpeechBubble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_goalSpeechBubble = transform.Find("GoalSpeechBubble").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            m_goalSpeechBubble.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            m_goalSpeechBubble.SetActive(false);
+        }
+    }
+}
