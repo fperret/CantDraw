a330dbcb2840f8593b2814f0aeb49428a323e333
fperret <fperret@student.42.fr>
2020-06-06 18:06:47 +0200

    start setting up new level and sprite for cliff

--- Assets/Sea.cs
+++ Assets/Sea.cs
@@ -7,12 +7,21 @@ public class Sea : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine("animate");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, -0.1f * Time.deltaTime, 0);
+    }
+
+    private IEnumerator animate()
+    {
+        while (true)
+        {
+            transform.Translate(0, -0.1f * Time.deltaTime, 0);
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }
